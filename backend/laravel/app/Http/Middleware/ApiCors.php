5632eacfa474f8a8d2f430216a4951b868a086f2
aki-743 <akimu1273@i.softbank.jp>
2021-03-10 18:42:26 +0900

    0310-8

--- backend/laravel/app/Http/Middleware/ApiCors.php
+++ backend/laravel/app/Http/Middleware/ApiCors.php
@@ -19,13 +19,21 @@ class ApiCors
         // すべてのレスポンスに CORS 用のヘッダーを追加する必要はないので URL から判断する
         $paths = explode('/', $request->getPathInfo());
         if ($paths[1] === 'api') {
+            if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+                return $next($request)
+                    ->header('Access-Control-Allow-Origin', config('cors.allowed_origins'))
+                    ->header('Cache-Control', 'public')
+                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                    ->header('Access-Control-Allow-Credentials', 'true')
+                    ->header('Status', '204');
+            };
             return $next($request)
-            ->header('Access-Control-Allow-Origin', config('cors.allowed_origins'))
-            ->header('Cache-Control', 'public')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Credentials', 'true')
-            ->header('Access-Control-Allow-Headers', 'X-XSRF-TOKEN, Authorization, content-type, Transfer-Encoding, Accept, Accept-Encoding, Accept-Language')
-            ->header('Access-Control-Expose-Headers', 'Authorization');
+                ->header('Access-Control-Allow-Origin', config('cors.allowed_origins'))
+                ->header('Cache-Control', 'public')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Credentials', 'true')
+                ->header('Access-Control-Allow-Headers', 'X-XSRF-TOKEN, Authorization, content-type, Transfer-Encoding, Accept, Accept-Encoding, Accept-Language')
+                ->header('Access-Control-Expose-Headers', 'Authorization');
         }
         return $next($request);
     }
