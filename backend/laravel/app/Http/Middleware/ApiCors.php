23626cec13f78e1295740dc0e75bb344d6eb0323
aki-743 <akimu1273@i.softbank.jp>
2021-03-10 18:49:57 +0900

    0310-9

--- backend/laravel/app/Http/Middleware/ApiCors.php
+++ backend/laravel/app/Http/Middleware/ApiCors.php
@@ -24,6 +24,7 @@ class ApiCors
                     ->header('Access-Control-Allow-Origin', config('cors.allowed_origins'))
                     ->header('Cache-Control', 'public')
                     ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                    ->header('Access-Control-Allow-Headers', 'X-XSRF-TOKEN, Authorization, content-type, Transfer-Encoding, Accept, Accept-Encoding, Accept-Language')
                     ->header('Access-Control-Allow-Credentials', 'true')
                     ->header('Status', '204');
             };
